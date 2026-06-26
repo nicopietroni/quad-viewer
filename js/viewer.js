a5252b192f1e942c4daad7f3aac23a4ba98ee93b
Nico Pietroni <nico.pietroni@gmail.com>
2026-06-26 13:11:59 +0200

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -198,7 +198,7 @@ async function buildQuadGroup(url) {
       const geo = child.geometry.clone();
       geo.computeVertexNormals();
       const solidMat = new THREE.MeshStandardMaterial({
-        color: 0xc7cbc8,
+        color: 0xe2e4e2,
         flatShading: true,
         roughness: 1.0,
         metalness: 0.0,
